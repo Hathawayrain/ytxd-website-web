3eff41c077af845dfd03c9bb33f4dc82990a247a
Hathaway_rain <530948059@qq.com>
2019-08-22 14:15:42 +0800

    更新jqueryUI

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 /**
  * Created by lixinyu on 2019/8/21.
  */
+$(document).ready(function () {
+    //swiper轮播图
+    var swiper = new Swiper('.swiper-container1', {
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        autoplay: true,
+        observer: true,//修改swiper自己或子元素时，自动初始化swiper
+        observeParents: true//修改swiper的父元素时，自动初始化swiper
+    });
+    //导航条
+    $("#menu").menu();
+})
+
